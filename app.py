d8879ea7cfdaca0b0e13375e83de2045210282a3
Yovel Cohen <yovel.c@gloat.com>
2021-10-14 10:37:46 +0300

    added consts

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends
 
 from auth.jwt_bearer import JWTBearer
-from routes.student import router as UserRouter
+from routes.user import router as UserRouter
 from routes.admin import router as AdminRouter
 
 app = FastAPI()
@@ -11,7 +11,7 @@ token_listener = JWTBearer()
 
 @app.get("/", tags=["Root"])
 async def read_root():
-    return {"message": "Welcome to this fantastic app."}
+    return {"message": "Welcome"}
 
 
 app.include_router(AdminRouter, tags=["Administrator"], prefix="/admin")
